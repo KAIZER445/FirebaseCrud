561e87ac2b7961625353aeb0ba15168c0dcd1162
KAIZER445 <prashanttamang861@gmail.com>
2025-09-06 10:59:13 +0545

    successfully retrieved data from database

--- app/src/main/java/own/project/firebasecrud/MainActivity.kt
+++ app/src/main/java/own/project/firebasecrud/MainActivity.kt
@@ -3,11 +3,19 @@ package own.project.firebasecrud
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import own.project.firebasecrud.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mainBinding : ActivityMainBinding
+
+    val database :FirebaseDatabase = FirebaseDatabase.getInstance()
+    val myReference :DatabaseReference = database.reference.child("MyUsers")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +27,31 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddUserActivity::class.java)
             startActivity(intent)
         }
+
+        retrieveDataFromDatabase()
+    }
+
+
+    fun retrieveDataFromDatabase(){
+        myReference.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot){
+
+                for(eachUser in snapshot.children){
+                    val user = eachUser.getValue(Users::class.java)
+
+                    if (user!=null){
+                        println("UserID = ${user.userId}")
+                        println("UserName = ${user.userName}")
+                        println("UserAge = ${user.userAge}")
+                        println("UserEmail = ${user.userEmail}")
+                        println("*************************")
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError){
+
+            }
+        })
     }
 }
