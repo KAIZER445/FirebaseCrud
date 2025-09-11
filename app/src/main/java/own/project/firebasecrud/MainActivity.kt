37de4e4599796e21dc6d553fcac3d3c9f64fcd72
KAIZER445 <prashanttamang861@gmail.com>
2025-09-11 08:29:53 +0545

    data display in recyclerview

--- app/src/main/java/own/project/firebasecrud/MainActivity.kt
+++ app/src/main/java/own/project/firebasecrud/MainActivity.kt
@@ -3,6 +3,7 @@ package own.project.firebasecrud
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -16,6 +17,9 @@ class MainActivity : AppCompatActivity() {
 
     val database :FirebaseDatabase = FirebaseDatabase.getInstance()
     val myReference :DatabaseReference = database.reference.child("MyUsers")
+
+    val userList = ArrayList<Users>()
+    lateinit var usersAdapter : UsersAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,17 +39,18 @@ class MainActivity : AppCompatActivity() {
     fun retrieveDataFromDatabase(){
         myReference.addValueEventListener(object :ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot){
+                userList.clear()
 
                 for(eachUser in snapshot.children){
                     val user = eachUser.getValue(Users::class.java)
 
                     if (user!=null){
-                        println("UserID = ${user.userId}")
-                        println("UserName = ${user.userName}")
-                        println("UserAge = ${user.userAge}")
-                        println("UserEmail = ${user.userEmail}")
-                        println("*************************")
+                        userList.add(user)
                     }
+
+                    usersAdapter = UsersAdapter(this@MainActivity, userList)
+                    mainBinding.recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                    mainBinding.recyclerView.adapter = usersAdapter
                 }
             }
 
