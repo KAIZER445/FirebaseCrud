16e80322968ab3210b4b5041aebacc072cb9af7c
KAIZER445 <prashanttamang861@gmail.com>
2025-11-08 18:19:02 +0545

    completed user registration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("com.google.firebase:firebase-database:22.0.0")
+    implementation("com.google.firebase:firebase-auth-ktx:21.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
--- app/src/main/java/own/project/firebasecrud/SignupActivity.kt
+++ app/src/main/java/own/project/firebasecrud/SignupActivity.kt
@@ -2,13 +2,43 @@ package own.project.firebasecrud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import own.project.firebasecrud.databinding.ActivityLoginBinding
+import own.project.firebasecrud.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
 
-    lateinit var viewBinding: ActivityLoginBinding
+    lateinit var signupBinding: ActivitySignupBinding
+
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        signupBinding = ActivitySignupBinding.inflate(layoutInflater)
+        val view = signupBinding.root
+        setContentView(view)
+
+
+        signupBinding.signupUser.setOnClickListener {
+
+            val userEmail = signupBinding.editTextEmailSignUp.text.toString()
+            val userPassword = signupBinding.editTextPasswordSignUp.text.toString()
+
+            signUpWithFirebase(userEmail,userPassword)
+        }
+    }
+
+
+    fun signUpWithFirebase(userEmail:String, userPassword:String){
+
+        auth.createUserWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener{task->
+            if (task.isSuccessful){
+                Toast.makeText(applicationContext,"your account is created successfully",Toast.LENGTH_SHORT).show()
+                finish()
+            }else{
+                Toast.makeText(applicationContext,task.exception?.toString(),Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
