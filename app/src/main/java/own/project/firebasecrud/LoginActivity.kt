15b3454e36e2a4a154a20195c8adf2db1bf45272
KAIZER445 <prashanttamang861@gmail.com>
2025-11-09 14:44:34 +0545

    login sucessful

--- app/src/main/java/own/project/firebasecrud/LoginActivity.kt
+++ app/src/main/java/own/project/firebasecrud/LoginActivity.kt
@@ -3,11 +3,15 @@ package own.project.firebasecrud
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import own.project.firebasecrud.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var loginBinding: ActivityLoginBinding
+
+    val auth : FirebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         loginBinding = ActivityLoginBinding.inflate(layoutInflater)
@@ -17,13 +21,30 @@ class LoginActivity : AppCompatActivity() {
 
         loginBinding.login.setOnClickListener {
 
+            val userEmail = loginBinding.editTextEmailSignIn.text.toString()
+            val userPassword = loginBinding.editTextPasswordSignIn.text.toString()
+
+            signInWithFirebase(userEmail, userPassword)
         }
 
         loginBinding.signup.setOnClickListener {
 
             val intent = Intent(this@LoginActivity,SignupActivity::class.java)
             startActivity(intent)
-
         }
+    }
+
+    fun signInWithFirebase(userEmail : String, userPassword : String){
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                    Toast.makeText(applicationContext,"login successful",Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(applicationContext,task.exception?.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
